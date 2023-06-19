b46a9bbb713428f0fffec844ccf2791b2ef3fd21
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-06-19 13:06:49 +0300

    added a dependency to the video player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
- import React from 'react'
- 
- function App() {
-   return (
-     <div>
-       
-     </div>
-   )
- }
- 
- export default App
- 
+import React from 'react';
+import VideoPlayer from './VideoPlayer';
+
+function App() {
+  return (
+    <div>
+      <h1>My Video Player</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
 
 function VideoPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div>
-      
+      <video ref={videoRef} controls>
+        <source src="path_to_video_file.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <button onClick={handlePlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
     </div>
-  )
+  );
 }
 
-export default VideoPlayer
+export default VideoPlayer;
