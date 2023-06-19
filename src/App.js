b159276d23f03dc41c5bf6764518a8c21d24874a
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-06-19 15:50:53 +0300

    created a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
+import VideoPlayer from './components/VideoPlayer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <h1>My Video Player</h1>
       <VideoPlayer />
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <h3>GOJARA</h3>
+      
+    </div>
+  )
+}
+
+export default NavBar
